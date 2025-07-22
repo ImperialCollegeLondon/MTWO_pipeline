6e46712698153a075c493d6f833134ee2e23c3e6
Yufeng <yufengna820@gmail.com>
2025-07-22 08:10:42 +0800

    Update filter.py

--- dataTransformer/filter.py
+++ dataTransformer/filter.py
@@ -1,16 +1,11 @@
 import numpy as np
 from scipy.signal import butter, lfilter
 
-def butterworth_filter(data, cutoff=3.0, fs=50.0, order=2):
-    nyq = 0.5 * fs
-    normal_cutoff = cutoff / nyq
-    b, a = butter(order, normal_cutoff, btype='low', analog=False)
-    y = lfilter(b, a, data)
-    return y
-
-def filter(data, threshold=0.3):
-    data = butterworth_filter(data)
-    # Divide by two if the absolute value of acceleration is greater than threshold
-    # data = np.where(data > threshold, data / 2, data)
-    # data = np.where(data < -threshold, data / 2, data)
-    return data
+def filter(data, order=4, cutoff_freq=5, sampling_rate=200):
+    '''The meaning of the parameters of butter:
+    N=4 means the order of the filter
+    Wn=5 means the cutoff frequency is 5Hz.
+    fs=200 means the sampling frequency is 200Hz.'''
+    b, a = butter(order, [cutoff_freq], fs=sampling_rate)
+    filtered_data = lfilter(b, a, data)
+    return filtered_data
